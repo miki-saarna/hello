96d9a3ee646d144f3e818c32d96fc7dda4b5355c
Mikito Saarna <mikitosaarna@gmail.com>
2021-11-17 14:08:21 -0600

    update environment vars

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,14 @@
-const { PORT = 5000 } = process.env;
+const { BACKEND_PORT = 5000 } = process.env;
 
 const app = require("./app");
 const knex = require("./db/connection");
 
-const listener = () => console.log(`Listening on Port ${PORT}!`);
+const listener = () => console.log(`Listening on Port ${BACKEND_PORT}!`);
 
 knex.migrate
   .latest()
   .then((migrations) => {
     console.log("migrations", migrations);
-    app.listen(PORT, listener);
+    app.listen(BACKEND_PORT, listener);
   })
   .catch(console.error);
