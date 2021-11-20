7066985314d076d7c44cb1632f56bcc452da78d3
Mikito Saarna <mikitosaarna@gmail.com>
2021-11-19 20:27:03 -0600

    commit

--- backend 2/Theaters-Movies-backend/src/errors/errorHandler.js
+++
@@ -1,7 +0,0 @@
-function errorHandler(err, req, res, next) {
-    console.error(err);
-    const { status = 500, message = "Something went wrong!" } = err;
-    res.status(status).json({ error: message })
-}
-
-module.exports = errorHandler;
